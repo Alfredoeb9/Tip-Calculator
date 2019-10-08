19cf351837146dc89db1867ac732735ec95c41a6
Alfredoeb9 <alfredoeb96@gmail.com>
2019-10-07 22:50:01 -0700

    received input text

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('connected');
+const calcBtn = document.querySelector('#calculate')
+
+
+calcBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  let billAmt = document.getElementById('bill').value
+  console.log(billAmt);
+  
+})
