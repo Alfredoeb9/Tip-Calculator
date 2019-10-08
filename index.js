e0d944d1f6999c2e2a7f0fa7115e1ff5e81a45d9
Alfredoeb9 <alfredoeb96@gmail.com>
2019-10-08 13:12:19 -0700

    sertice option received and output tip onto page

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ calcBtn.addEventListener('click', (e) => {
   e.preventDefault();
   
   let billAmt = document.getElementById('bill').value
-  console.log(billAmt);
+  let serviceOpt = document.getElementById('serviceOpt').value
+  let tipPlaceholder = document.getElementById('tip')
+
+  let total = billAmt * serviceOpt
+
+  tipPlaceholder.style.display = 'block'
+
+  tipPlaceholder.innerText = total
+  console.log(billAmt * serviceOpt);
+  console.log(serviceOpt);
   
 })
